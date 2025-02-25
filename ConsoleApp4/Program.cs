da4aef928483402bffa049d49e84b30912c39320
Ata <atadevels@gmail.com>
2025-02-25 18:33:58 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Cas Latency (CL) hizini giriniz: ");
+        int cas = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Megatransfer(Mt/s) hizini giriniz: ");
+        int megatransfer = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(calclatency(megatransfer, cas));
+    }
+    private static double calclatency(int megatransfer ,int cas)
+    {
+        double latency = cas *2000/megatransfer;
+        return latency;
+
+    }
+
+}
